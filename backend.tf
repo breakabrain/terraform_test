992a8691070ae678aca00cebe7e9356f2b748a47
breakabrain <breakabrain@pm.me>
2020-02-11 13:06:07 +0500

    Add variables for backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,3 @@
 terraform {
   backend "s3" {}
 }
-
-data "terraform_remote_state" "state" {
-  backend = "s3"
-  config = {
-    bucket = "${var.bucket_name}"
-    key    = "${var.key_file}"
-    region = "${var.region_bucket}"
-  }
-}
