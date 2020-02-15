b94cb30efb062bf033968ea184b9797da8b59a47
Pavel <breakabrain@pm.me>
2020-02-15 16:48:48 +0500

    Used terraform fmt

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 provider "aws" {
-    region = var.region[terraform.workspace]
+  region = var.region[terraform.workspace]
 }
 
 resource "aws_key_pair" "key_for_bastion" {
-    key_name = "key_for_bastion"
-    public_key = file(var.ssh_key[terraform.workspace])
-    tags = {
-        Name = terraform.workspace-terraform
-    }
+  key_name   = "key_for_bastion"
+  public_key = file(var.ssh_key[terraform.workspace])
+  tags = {
+    Name = terraform.workspace-terraform
+  }
 }
 
 resource "aws_eip" "eip_bastion" {
-    vpc = true
-    tags = {
-        Name = terraform.workspace-terraform
-    }
+  vpc = true
+  tags = {
+    Name = terraform.workspace-terraform
+  }
 }
 
 resource "aws_iam_role" "ir_bastion" {
-  name = "ir_bastion"
+  name               = "ir_bastion"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -36,8 +36,8 @@ EOF
 }
 
 resource "aws_iam_role_policy" "irp_bastion" {
-  name = "irp_bastion-EIPAttachPolicy"
-  role = aws_iam_role.ir_bastion.name
+  name   = "irp_bastion-EIPAttachPolicy"
+  role   = aws_iam_role.ir_bastion.name
   policy = <<EOF
 {
       "Version": "2012-10-17",
@@ -60,56 +60,56 @@ resource "aws_iam_instance_profile" "iip_bastion" {
 }
 
 resource "aws_security_group" "ssh-only" {
-    name = "ssh-only"
-    description = "ssh-only"
-    vpc_id = var.vpc_id[terraform.workspace]
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = {
-        Name = terraform.workspace-terraform
-    }
+  name        = "ssh-only"
+  description = "ssh-only"
+  vpc_id      = var.vpc_id[terraform.workspace]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = terraform.workspace-terraform
+  }
 }
 
 resource "aws_launch_configuration" "lc_bastion" {
-    name = "lc_bastion"
-    image_id = var.image_id[terraform.workspace]
-    instance_type = var.instance_type[terraform.workspace]
-    iam_instance_profile = aws_iam_instance_profile.iip_bastion.name
-    associate_public_ip_address = false
-    security_groups = [aws_security_group.ssh-only.id]
-    enable_monitoring = false
-    lifecycle {
-        create_before_destroy = true
-    }
-    key_name = aws_key_pair.key_for_bastion.key_name
-    user_data = templatefile("user_data.sh", {
-        eip = aws_eip.eip_bastion.id,
-        region = var.region[terraform.workspace]
-    })
+  name                        = "lc_bastion"
+  image_id                    = var.image_id[terraform.workspace]
+  instance_type               = var.instance_type[terraform.workspace]
+  iam_instance_profile        = aws_iam_instance_profile.iip_bastion.name
+  associate_public_ip_address = false
+  security_groups             = [aws_security_group.ssh-only.id]
+  enable_monitoring           = false
+  lifecycle {
+    create_before_destroy = true
+  }
+  key_name = aws_key_pair.key_for_bastion.key_name
+  user_data = templatefile("user_data.sh", {
+    eip    = aws_eip.eip_bastion.id,
+    region = var.region[terraform.workspace]
+  })
 }
 
 resource "aws_autoscaling_group" "ag_bastion" {
-    name = "ag_bastion"
-    max_size = 1
-    min_size = 1
-    desired_capacity = 1
-    health_check_type = "EC2"
-    launch_configuration = aws_launch_configuration.lc_bastion.name
-    vpc_zone_identifier = var.subnet_ids[terraform.workspace]
-    default_cooldown = "10"
-    tag  {
-        key = "Name"
-        value = terraform.workspace-terraform
-        propagate_at_launch = true
-    }
+  name                 = "ag_bastion"
+  max_size             = 1
+  min_size             = 1
+  desired_capacity     = 1
+  health_check_type    = "EC2"
+  launch_configuration = aws_launch_configuration.lc_bastion.name
+  vpc_zone_identifier  = var.subnet_ids[terraform.workspace]
+  default_cooldown     = "10"
+  tag {
+    key                 = "Name"
+    value               = terraform.workspace-terraform
+    propagate_at_launch = true
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,50 +1,50 @@
 ################### variables
 variable "region" {
-    default = {
-        testing = "eu-central-1"
-        staging = "eu-west-1"
-        production = "eu-west-2"
-    }
+  default = {
+    testing    = "eu-central-1"
+    staging    = "eu-west-1"
+    production = "eu-west-2"
+  }
 }
 
 variable "vpc_id" {
-    default = {
-        testing = "vpc-9a8b49f0"
-        staging = "vpc-55bd512c"
-        production = "vpc-38dca250"
-    }
+  default = {
+    testing    = "vpc-9a8b49f0"
+    staging    = "vpc-55bd512c"
+    production = "vpc-38dca250"
+  }
 }
 
 variable "subnet_ids" {
-    default = {
-        testing = ["subnet-b019f8cc", "subnet-1b15e157"]
-        staging = ["subnet-082c0540", "subnet-a5940fff"]
-        production = ["subnet-2beeae42", "subnet-b5cd1ff9"]
-    }
+  default = {
+    testing    = ["subnet-b019f8cc", "subnet-1b15e157"]
+    staging    = ["subnet-082c0540", "subnet-a5940fff"]
+    production = ["subnet-2beeae42", "subnet-b5cd1ff9"]
+  }
 }
 
 variable "instance_type" {
-    default = {
-        testing = "t2.micro"
-        staging = "t2.micro"
-        production = "t2.micro"
-    }
+  default = {
+    testing    = "t2.micro"
+    staging    = "t2.micro"
+    production = "t2.micro"
+  }
 }
 
 variable "image_id" {
-    default = {
-        testing = "ami-07cda0db070313c52"
-        staging = "ami-0713f98de93617bb4"
-        production = "ami-0089b31e09ac3fffc"
-    }
+  default = {
+    testing    = "ami-07cda0db070313c52"
+    staging    = "ami-0713f98de93617bb4"
+    production = "ami-0089b31e09ac3fffc"
+  }
 }
 
 variable "ssh_key" {
-    description = "public ssh key for bastion"
-    default = {
-        testing = "./testing-key-openssh.pub"
-        staging = "./staging-key-openssh.pub"
-        production = "./production-key-openssh.pub"
-    }
+  description = "public ssh key for bastion"
+  default = {
+    testing    = "./testing-key-openssh.pub"
+    staging    = "./staging-key-openssh.pub"
+    production = "./production-key-openssh.pub"
+  }
 }
 ####################### end of list variables
